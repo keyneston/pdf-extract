069272f87a70aed8e6d677ee3e1e3bf69a07ff73
Tabitha <tabitha@keyneston.com>
2021-05-07 20:03:15 +0200

    Templated page serving images

--- name-extracted/main.go
+++ name-extracted/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net"
 	"net/http"
@@ -12,13 +13,40 @@ import (
 	"strconv"
 )
 
+var tmpl = template.Must(template.New("rename").Parse(`
+<html>
+<body>
+<h1>Page {{.page}}</h1>
+<img src="/images/{{.path}}">
+<ul>
+{{range $i, $a := .text}}
+<li>Title: {{$a}}</li>
+{{end}}
+</ul>
+</body>
+</html>
+`))
+
 type Server struct {
 	files []string
 	text  map[int][]string
 }
 
 func (s *Server) handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	var page, id int
+	//q := r.URL.Query()
+
+	f := s.files[50]
+	if _, err := fmt.Sscanf(f, "page_%d_id_%d.png", &page, &id); err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+	}
+
+	tmpl.ExecuteTemplate(w, "rename", map[string]interface{}{
+		"page": page,
+		"id":   id,
+		"path": f,
+		"text": s.text[page],
+	})
 }
 
 func (s *Server) parseDirectory(dir string) error {
@@ -53,8 +81,19 @@ func main() {
 
 	s := Server{}
 	s.parseDirectory("/tmp/tokens")
-	log.Fatalf("%#v", s)
 
-	http.HandleFunc("/", s.handler)
-	log.Fatal(http.ListenAndServe(net.JoinHostPort("", strconv.Itoa(port)), nil))
+	mux := http.NewServeMux()
+	mux.Handle(
+		"/images/",
+		http.StripPrefix("/images/",
+			http.FileServer(http.Dir("/tmp/tokens"))))
+	mux.HandleFunc("/", s.handler)
+	log.Fatal(http.ListenAndServe(net.JoinHostPort("", strconv.Itoa(port)), logRequest(mux)))
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
 }
